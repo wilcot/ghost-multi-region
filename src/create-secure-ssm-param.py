6dc1dcc4c6aec033cbf72762e1ec68aece97a2eb
wilcot <wilcot@users.noreply.github.com>
2017-08-29 21:54:51 -0500

    moved logic into function

--- src/create-secure-ssm-param.py
+++ src/create-secure-ssm-param.py
@@ -1,29 +1,32 @@
+import boto3
+#import cfnresponse
+ssm = boto3.client('ssm')
+def create_ssm_secure_parameter(Name,Description,Value):
+    try:
+        ssm.put_parameter(
+                Name=Name,
+                Description=Description,
+                Value=Value,
+                Type='SecureString',
+                Overwrite=True
+            )
+        print(f"created secure ssm parameter: \"{Name}\"")
+    except Exception as e:
+        raise Exception(f"unable to create parameter: \"{Name}\". error:" + str(e))
+
+def delete_ssm_parameter(Name):
+    try:
+        ssm.delete_parameter(Name=Name)
+        print(f"deleted parameter: {Name}")
+    except Exception as e: 
+        if 'ParameterNotFound' in str(e):
+            print(f"called delete_parameter for \"{Name}\", but the ssm parameter does not exist")
+        else:
+            raise Exception(f"unable to delete parameter \"{Name}\". error:" + str(e)) 
+
 def lambda_handler(event, context):
-    # TODO implement
-    import boto3
-    #import cfnresponse
-    ssm = boto3.client('ssm')
-    ssm.put_parameter
     if event['request'] == 'delete':
-        try:
-            ssm.delete_parameter(
-                    Name=event['name']
-                )
-            print(f"deleted parameter: {event['name']}")
-        except Exception as e: 
-            if 'ParameterNotFound'  in str(e):
-                print(f"tried to delete parameter: {event['name']} but it did not exist")
-            else:
-                raise Exception(f"unable to delete parameter: {event['name']}. error:" + str(e))
+        delete_ssm_parameter(event['name'])
     else:
-        try:
-            ssm.put_parameter(
-                    Name=event['name'],
-                    Description=event['description'],
-                    Value=event['value'],
-                    Type='SecureString',
-                    Overwrite=True
-                )
-            print(f"created secure parameter: {event['name']}")
-        except Exception as e:
-            raise Exception(f"unable to create parameter: {event['name']}. error:" + str(e))
+        create_ssm_secure_parameter(event['name'],event['description'],event['value'])
+        
